52e15243fc73b4ba5770e0768a53edf4f86e25aa
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-04-09 15:40:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main()
     dlist object;
     object.build();
     object.display();
-    object.remove(9);
+    node* current = head;
+    object.remove(current, 9);
 
     //PLEASE PUT YOUR CODE HERE to call the function assigned
 
