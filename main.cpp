9a64d37b7521b76d3f049e3759c2a462288a8fad
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-04-11 08:09:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,7 @@ int main()
     
     node* head1 = object.getHead();
     node* current = head1;
-    node* copyN = NULL;
-    object.remove(head1, head1, copyN, copyN, 2);
+    object.remove(head1, 2);
 
     //PLEASE PUT YOUR CODE HERE to call the function assigned
 
