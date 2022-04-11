484c516194e9c9801645ffb09f645b5d84ad8515
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-04-10 22:50:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     object.display();
     node* head1 = object.getHead();
     node* current = head1;
-    object.remove(current,3);
+    object.remove(head1, current,3);
 
     //PLEASE PUT YOUR CODE HERE to call the function assigned
 
