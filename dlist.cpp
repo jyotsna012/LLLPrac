f59d17a4325e5be3d9d2f44c087a0272655071c8
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-04-10 23:08:37 -0700

    Update dlist.cpp

--- dlist.cpp
+++ dlist.cpp
@@ -53,8 +53,8 @@ void dlist::remove(node* &head, node* current, int th){
   remove(head, current, th);
   if(current->data >= th){
     cout << "over 5: " << current-> data << endl;
-      if(current == head){
-        cout<<"head"<<endl;
+      if(current -> == NULL){
+        cout<<"head: "  << current-> data <<endl;
         node* temp = head;
         head = head->next;
       }
